f5397c5121b03e6212a8234fe900f80f4db4df4f
ibrahimyeler <ibrahimyeler10@gmail.com>
2024-08-12 09:34:31 +0300

    create sign up page ui-2

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function SignUp() {
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* Sol taraf */}
         <div className="flex-1">
           <Link to='/' className='font-bold dark:text-white'>
@@ -46,9 +46,7 @@ export default function SignUp() {
               
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit" >
-              Sign Up
-              </Button>
+        <Button gradientDuoTone='purpleToPink' type="submit"> Sign Up</Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
